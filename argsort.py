bb90df3d1c8483ae2d876637e0b3b14ac92f1072
Andrew D. Yates <andrewyates.name@gmail.com>
2012-06-28 22:27:21 -0400

    bug fix

--- argsort.py
+++ argsort.py
@@ -7,5 +7,5 @@ M = np.load(sys.argv[1])
 print "Argsorting %s..." % m_fname
 Q = np.argsort(M)
 m_fname_argsort = m_fname + ".argsorted.npy"
-print "Saving %s as %s..." % m_fname_argsort
+print "Saving argsorted as %s..." % m_fname_argsort
 np.save(m_fname_argsort, Q)
