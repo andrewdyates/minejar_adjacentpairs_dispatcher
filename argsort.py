adb8db5d8b52bff99905d63d06b734c8ae04b573
Andrew D. Yates <andrewyates.name@gmail.com>
2012-06-28 23:00:13 -0400

    pychecker

--- argsort.py
+++ argsort.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python
 import numpy as np
 import sys
-m_fname = sys.argv[1]
-print "Loading %s..." % m_fname
-M = np.load(sys.argv[1])
-print "Argsorting %s..." % m_fname
-Q = np.argsort(M)
-m_fname_argsort = m_fname + ".argsorted.npy"
-print "Saving argsorted as %s..." % m_fname_argsort
-np.save(m_fname_argsort, Q)
+
+def main()
+  m_fname = sys.argv[1]
+  print "Loading %s..." % m_fname
+  M = np.load(sys.argv[1])
+  print "Argsorting %s..." % m_fname
+  Q = np.argsort(M)
+  m_fname_argsort = m_fname + ".argsorted.npy"
+  print "Saving argsorted as %s..." % m_fname_argsort
+  np.save(m_fname_argsort, Q)
+  
+if __name__ == "__main__":
+  main()
