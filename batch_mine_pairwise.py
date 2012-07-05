36d57a33dfde5fe6ee53b0c3f9e94337ba44d296
Andrew D. Yates <andrewyates.name@gmail.com>
2012-07-05 11:32:14 -0400

    docs, bound checking

--- batch_mine_pairwise.py
+++ batch_mine_pairwise.py
@@ -2,6 +2,9 @@
 """Batchfile for computing a list of pairwise MINE.jar rows.
 
 Assume that tab_fname variable order corresponds to Boolean matrix.
+
+SAMPLE USE:
+python batch_mine_pairwise.py tab_fname=/fs/lustre/osu6683/GSE2034.GPL96.eQTL.normed.tab bool_fname=/fs/lustre/osu6683/GSE2034.GPL96.eQTL.normed.tab.varlist.txt.MINE.isset.npy offset=0 k=10000 
 """
 import sys
 import subprocess
@@ -12,9 +15,11 @@ from py_symmetric_matrix import *
 
 def main(tab_fname=None, bool_fname=None, offset=0, k=200000, work_dir='/fs/lustre/osu6683', minejar_path="/fs/lustre/osu6683/MINE.jar"):
   offset = int(offset)
+  k = int(k)
   assert tab_fname
   assert bool_fname
   assert k
+  assert k > 0 and offset >= 0
 
   # read tabfile as {varname => str}
   M = {}
@@ -36,6 +41,9 @@ def main(tab_fname=None, bool_fname=None, offset=0, k=200000, work_dir='/fs/lust
     print "Offset %d is greater than number of missing pairs %d. Exiting." % \
       (offset, len(Q))
     return 
+  if k+offset > len(Q):
+    k = len(Q)-offset
+    print "k+offset greater than number of pairs. Setting k to %d." % k
 
   fname = os.path.basename(tab_fname).rpartition('.')[0] + "_offset_%d_%d.tab"%\
      (offset, k)
